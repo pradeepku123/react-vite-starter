ea56cd7038fbdbd27df3b590af3835a2efcd6d23
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-10-09 23:50:30 +0530

    react hook starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 function App() {
+  let [counter, setCounter] = useState(0);
+  let addCounter = () => {
+    counter++;
+    console.log(`counter: ${counter}`);
+    setCounter(counter);
+  };
+  let deleteCounter = () => {
+    counter--;
+    console.log(`counter: ${counter}`);
+    setCounter(counter);
+  };
   return (
     <>
-      <h1> Hellow World</h1>
+      <h1> Counter Value: {counter}</h1>
+      <button onClick={addCounter}>Add</button>
+      <button onClick={deleteCounter}>Delete</button>
     </>
   );
 }
